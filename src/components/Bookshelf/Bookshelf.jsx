a0fcaa9abac81523292a2780c407be3e93195de7
FahadQ <Fhdq2003@email.com>
2025-08-10 16:24:25 +0300

    added handleInputChange and handleSubmit methods to Bookshelf component

---
+++ src/components/Bookshelf/Bookshelf.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+
+function Bookshelf()
+{
+    const [books, setBooks] = useState(
+    [
+        { title: 'Fourth Wing', author: 'Rebecca Yarros' },
+        { title: 'The Lion, the Witch and the Wardrobe', author: 'C.S. Lewis' },
+    ]);
+    
+    const [newBook, setNewBook] = useState(
+        {
+            title: "",
+            author: ""
+        }
+    )
+
+    function handleInputChange(event)
+    {
+        setNewBook({...newBook, [event.target.name]: event.target.value})
+    }
+
+    function handleSubmit(event)
+    {
+        event.preventDefault();
+
+        const copyBooks = [...books, newBook];
+        setNewBook({ title: "", author: "" });
+        
+        setBooks(copyBooks);
+        console.log(books);
+    }
+
+    return (
+        <div className="bookshelfDiv">
+            <div className="formDiv">
+                <h3>Add a Book</h3>
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor='title'>Title: </label>
+                    <input name='title' onChange={handleInputChange} value={newBook.title}></input>
+                    <label htmlFor='author'>Author: </label>
+                    <input name='author' onChange={handleInputChange} value={newBook.author}></input>
+
+                    <button type='submit'>Add Book</button>
+                </form>
+            </div>
+            <div className="bookCardsDiv">
+                {
+                    /* Book cards will display here */
+                }
+            </div>
+        </div>
+    )
+}
+
+export default Bookshelf;
